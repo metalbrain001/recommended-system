11eb154ef6787214294926546cba77373a253757
Babatunde Kalejaiye <metal@metalbrain.net>
2024-10-05 23:59:49 +0100

    Added Github action

--- app/manage.py
+++ app/manage.py
@@ -3,11 +3,12 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
-        from django.core.management import execute_from_command_line # type: ignore
+        from django.core.management import execute_from_command_line  # type: ignore
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -16,5 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
