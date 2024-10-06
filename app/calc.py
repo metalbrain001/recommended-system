96f182cdac273728cdea53ac0292371448aaddc4
Babatunde Kalejaiye <metal@metalbrain.net>
2024-10-06 05:16:10 +0100

    Added DataBase/Psycopg2

--- app/calc.py
+++ app/calc.py
@@ -2,11 +2,12 @@
 This is a calculator app 
 """
 
-
 def add(x, y):
     """Add two numbers together"""
+    
     return x + y
   
 def subtract(x, y):
     """Subtract x from y and return the result"""
+    
     return y - x
--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,7 @@
 """ 
 Test cases for the app module. 
 """
-
 from django.test import SimpleTestCase
-
 from app import calc
 
 
@@ -13,10 +11,12 @@ class CalcTests(SimpleTestCase):
     
     def test_add(self):
         """ Test the add function. """
+        
         res = calc.add(3, 8)
         self.assertEqual(res, 11)
         
     def test_subtract(self):
         """ Test the subtract function. """
+       
         res = calc.subtract(6, 11)
         self.assertEqual(res, 5)
